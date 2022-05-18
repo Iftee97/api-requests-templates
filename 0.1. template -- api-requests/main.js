11ea371daebfeb5d0a7f2356c8c2bdea757ab8a3
Tawsif Haque <68259102+Iftee97@users.noreply.github.com>
2022-05-18 09:46:13 +0600

    Add files via upload

---
+++ 0.1. template -- api-requests/main.js
@@ -0,0 +1,41 @@
+// // example with json placeholder api of users
+
+// // with fetch (promises)
+const getUsersByFetch = () => {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => console.log("fetch api data (users):", data))
+        .catch((err) => console.log(err));
+};
+getUsersByFetch();
+
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+
+
+// // with fetch (async-await)
+// const getUsersByFetch2 = async () => {
+//     const url = "https://jsonplaceholder.typicode.com/users";
+//     try {
+//         const response = await fetch(url);
+//         const data = await response.json();
+//         console.log("fetch api data (users):", data);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// };
+// getUsersByFetch2();
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+
+
+// // with axios (async-await)
+// const getUsersByAxios = async () => {
+//     const url = "https://jsonplaceholder.typicode.com/users";
+//     const response = await axios.get(url); // note that axios has been included in the html file with a cdn
+//     console.log("axios data: (users):", response.data);
+// };
+// getUsersByAxios();
