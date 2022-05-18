8273ce06e82c771e808e412c448091a0359856ba
Tawsif Haque <68259102+Iftee97@users.noreply.github.com>
2022-05-18 09:49:45 +0600

    Update main.js

--- 0.1. template -- api-requests/main.js
+++ 0.1. template -- api-requests/main.js
@@ -16,26 +16,27 @@ getUsersByFetch();
 
 
 // // with fetch (async-await)
-// const getUsersByFetch2 = async () => {
-//     const url = "https://jsonplaceholder.typicode.com/users";
-//     try {
-//         const response = await fetch(url);
-//         const data = await response.json();
-//         console.log("fetch api data (users):", data);
-//     } catch (err) {
-//         console.log(err);
-//     }
-// };
-// getUsersByFetch2();
+const getUsersByFetch2 = async () => {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log("fetch api data (users):", data);
+    } catch (err) {
+        console.log(err);
+    }
+};
+getUsersByFetch2();
+
 
 // ------------------------------------------------------------------
 // ------------------------------------------------------------------
 
 
 // // with axios (async-await)
-// const getUsersByAxios = async () => {
-//     const url = "https://jsonplaceholder.typicode.com/users";
-//     const response = await axios.get(url); // note that axios has been included in the html file with a cdn
-//     console.log("axios data: (users):", response.data);
-// };
-// getUsersByAxios();
+const getUsersByAxios = async () => {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    const response = await axios.get(url); // note that axios has been included in the html file via cdn
+    console.log("axios data: (users):", response.data);
+};
+getUsersByAxios();
